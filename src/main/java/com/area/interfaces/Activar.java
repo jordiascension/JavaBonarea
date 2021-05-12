ba367a0eab6b1c4d85f967e827326c875614f981
jordiascension <jordiascension@enterpriseconsulting.es>
2021-05-12 02:01:58 +0200

    Implementació d'interfícies

---
+++ src/main/java/com/area/interfaces/Activar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.area.interfaces;
+
+/**
+ *
+ * @author jordi
+ */
+public interface Activar {
+    
+    public void on();
+    public void off();
+}
--- src/main/java/com/area/miprimerprograma/Principal.java
+++ src/main/java/com/area/miprimerprograma/Principal.java
@@ -6,28 +6,34 @@
 package com.area.miprimerprograma;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 /**
  *
  * @author jordi
  */
 public class Principal {
-    
+
     //Disable maven index update frecuency
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        
-        System.out.println("El resultado de la suma es " + 
-                calculadora.suma(4, 2));     
-        
-        System.out.println("El resultado de la suma con decimales es " +
-                calculadora.suma(new BigDecimal("1.505"), new BigDecimal("1.440"))); 
-        
-        BigDecimal num1= new BigDecimal("1.505");
-        BigDecimal num2= new BigDecimal("1.440");
+
+        System.out.println("El resultado de la suma es "
+                + calculadora.suma(4, 2));
+
+        System.out.println("El resultado de la suma con decimales es "
+                + calculadora.suma(new BigDecimal("1.505"), new BigDecimal("1.440")));
+
+        BigDecimal num1 = new BigDecimal("1.505");
+        BigDecimal num2 = new BigDecimal("1.440");
         //1.945--1.94
         //1.955--1.96
-        System.out.println("El resultado de la suma con decimales es " +
-                calculadora.suma(num1, num2)); 
+        System.out.println("El resultado de la suma con decimales es "
+                + calculadora.suma(num1, num2));
+
+        //Configuración del separador de miles
+        DecimalFormat formatea = new DecimalFormat("###,###.##");
+        System.out.println("El resultado de la suma con decimales es "
+                + formatea.format(calculadora.suma(num1, num2)));
     }
 }
